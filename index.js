3c9bb7543ede0160df7422d2b43595ff2d35b0c6
kutyel <flaviocorpa@gmail.com>
2017-07-22 13:40:47 +0200

    Solved in 7 lines of code 😎

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export default class Emitter {
-    constructor() { }
+  e = new Map()
+  subscribe = (n, cb) => (
+    this.e.set(n, [...this.e.has(n) ? this.e.get(n) : [], cb]),
+    () => this.e.set(n, this.e.get(n).filter(f => f !== cb)))
+  emit = (n, ...a) => this.e.has(n) && this.e.get(n).map(f => f(...a))
 }
