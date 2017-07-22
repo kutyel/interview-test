3504165fa4d575bced307c12d05dcc5cfc23b6cc
kutyel <flaviocorpa@gmail.com>
2017-07-22 12:26:48 +0200

    Finish step 2 🎉🎉

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ export default class Emitter {
         this.events.set(name, [
             ...this.events.has(name) ? this.events.get(name) : [], cb
         ])
+
+        return () => this.events.set(
+            name,
+            this.events.get(name).filter(fn => fn !== cb)
+        )
     }
 
     emit(name, ...args) {
