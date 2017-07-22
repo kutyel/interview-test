a2672b0bb28df12dccafe3a68ed792cb748ea1e5
kutyel <flaviocorpa@gmail.com>
2017-07-22 12:22:29 +0200

    Finish step 1 🎉

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 export default class Emitter {
-    constructor() { }
+    constructor() {
+        this.events = new Map()
+    }
+
+    subscribe(name, cb) {
+        this.events.set(name, [
+            ...this.events.has(name) ? this.events.get(name) : [], cb
+        ])
+    }
+
+    emit(name, ...args) {
+        this.events.has(name) && this.events.get(name).forEach(fn => fn(...args))
+    }
 }
